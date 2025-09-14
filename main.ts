b78cdb6fc73a68c0ddec109ad4550fdf9bcfbd8e
Brendan-Orourke <brendan.orourke@stu.ocsb.ca>
2025-09-13 20:47:24 -0400

    Calculates area and perimeter

--- main.ts
+++ main.ts
@@ -2,21 +2,19 @@
  *
  * Created by: Brendan O'Rourke
  * Created on: Sep 2025
- * This program Calculated Area and Perimeter
-*/
+ * This program calculates area and perimeter
+ */
+
+let length = 5;  // in cm
+let width = 3;   // in cm
 
 
-let length = 5  // in cm
-let width = 3   // in cm
+let area = length * width;
+let perimeter = 2 * (length + width);
 
-// Calculate area and perimeter
-let area = length * width
-let perimeter = 2 * (length + width)
 
-// Display area
-basic.showString("A:")
-basic.showNumber(area)
+basic.showString("A:");
+basic.showNumber(area);
 
-// Display perimeter
-basic.showString("P:")
-basic.showNumber(perimeter)
+
+basic.showString("P:");
