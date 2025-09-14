bb5f30fdab0304828313e48f6b22fc65d21c2c1e
Brendan-Orourke <brendan.orourke@stu.ocsb.ca>
2025-09-13 20:29:30 -0400

    Calculated area and perimeter

--- main.ts
+++ main.ts
@@ -5,4 +5,18 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// Declare variables only once
+let length = 5  // in cm
+let width = 3   // in cm
+
+// Calculate area and perimeter
+let area = length * width
+let perimeter = 2 * (length + width)
+
+// Display area
+basic.showString("A:")
+basic.showNumber(area)
+
+// Display perimeter
+basic.showString("P:")
+basic.showNumber(perimeter)
