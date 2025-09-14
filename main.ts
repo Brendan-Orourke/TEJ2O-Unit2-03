72d08fca37c014ae8ab84657b30b0465b2978f75
Brendan-Orourke <brendan.orourke@stu.ocsb.ca>
2025-09-13 20:53:42 -0400

    Calculates area and perimeter

--- main.ts
+++ main.ts
@@ -8,13 +8,11 @@
 let length = 5;  // in cm
 let width = 3;   // in cm
 
-
 let area = length * width;
 let perimeter = 2 * (length + width);
-
 
 basic.showString("A:");
 basic.showNumber(area);
 
-
 basic.showString("P:");
+basic.showNumber(perimeter);;
